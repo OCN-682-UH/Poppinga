1474d4882139e8df2d472dd26611da52a7bc1578
haleypoppinga <hpopping@hawaii.edu>
2025-09-09 17:45:30 -1000

    I am adding Week 04

---
+++ Week_04/Scripts/Week04_script.R
@@ -0,0 +1,11 @@
+### Data Wrangling (dplyr and tidyr)
+### Created by: Haley Poppinga
+### Created on: 2025_09_16
+###########################################
+
+### Load libraries ########## 
+library(tidyverse)
+library(here)
+library(ggplot2)
+
+### Read in data ###
