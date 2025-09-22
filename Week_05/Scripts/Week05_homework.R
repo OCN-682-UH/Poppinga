93dccda04ce5d15cabd77e515d5a275e212db790
haleypoppinga <hpopping@hawaii.edu>
2025-09-22 10:37:13 -1000

    update

---
+++ Week_05/Scripts/Week05_homework.R
@@ -0,0 +1,20 @@
+### Relational Data & Dates/Times (Lubridate) HOMEWORKS ###
+### Created by: Haley Poppinga
+### Created on: 2025_09_23
+###########################################
+
+#Homework 5a
+
+
+### Load libraries ########## 
+library(tidyverse)
+library(here)
+library(ggplot2)
+library(ghibli) 
+library(ggthemes)
+
+### Read in data ###
+
+
+
+### Data Analysis ###
