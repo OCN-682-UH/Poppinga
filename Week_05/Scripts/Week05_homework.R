397e3fd8f58737a9293e61af954676ab7bb692f4
haleypoppinga <hpopping@hawaii.edu>
2025-09-23 11:19:36 -1000

    update week05

--- Week_05/Scripts/Week05_homework.R
+++ Week_05/Scripts/Week05_homework.R
@@ -4,6 +4,16 @@
 ###########################################
 
 #Homework 5a
+# Read in both the conductivity and depth data.
+# Convert date columns appropriately
+# Round the conductivity data to the nearest 10 seconds so that it matches with the depth data
+# Join the two dataframes together (in a way where there will be no NAs...) 
+# (i.e. join in a way where only exact matches between the two dataframes are kept)
+# take averages of date, depth, temperature, and salinity by minute
+# Make any plot using the averaged data
+# Do the entire thing using mostly pipes (i.e. you should not have a bunch of different dataframes). Keep it clean.
+# Don't forget to comment!
+# Save the output, data, and scripts appropriately
 
 
 ### Load libraries ########## 
@@ -12,6 +22,9 @@ library(here)
 library(ggplot2)
 library(ghibli) 
 library(ggthemes)
+library(lubridate)
+
+# install.packages("lubridate") # package to deal with dates and times
 
 ### Read in data ###
 
--- Week_05/Scripts/Week05_script.R
+++ Week_05/Scripts/Week05_script.R
@@ -4,6 +4,10 @@
 ###########################################
 
 ### Week 5b Lecture ###
+### Today we are going to practice joins with data from Becker and Silbiger (2020) ####
+### Created by: Haley Poppinga #############
+### Created on: 2025-09-23 ####################
+
 
 ### Load libraries ###
 library(tidyverse)
@@ -12,20 +16,85 @@ library(ggplot2)
 library(ghibli) 
 
 ### Read in data ###
+### Load data ######
+# Environmental data from each site
+EnviroData<-read_csv(here("Week_05","data", "site.characteristics.data.csv"))
+
+#Thermal performance data
+TPCData<-read_csv(here("Week_05","data","Topt_data.csv"))
+glimpse(EnviroData)
+glimpse(TPCData)
+# also use View()
+
+#Pivot the data
+EnviroData_wide <- EnviroData %>% 
+  pivot_wider(names_from = parameter.measured,
+              values_from = values)
+View(EnviroData_wide)
+
+#Sort the data
+EnviroData_wide <- EnviroData %>% 
+  pivot_wider(names_from = parameter.measured, # pivot the data wider
+              values_from = values) %>%
+  arrange(site.letter) # arrange the dataframe by site
+View(EnviroData_wide)
 
 
+#left_join(x, y)
+FullData_left<- left_join(TPCData, EnviroData_wide)
+## Joining with by = join_by(site.letter)
+head(FullData_left)
 
 
+#relocate
+FullData_left<- left_join(TPCData, EnviroData_wide) %>%
+  relocate(where(is.numeric), .after = where(is.character)) # relocate all the numeric data after the character data
+## Joining with by = join_by(site.letter)
+head(FullData_left)
+
+#  calculate the mean and variance of all collected (TPC and environmental) data by site. 
+# (Hint: you can either use one of the summarise_at() functions or pivot the data longer to do this in less code)
+
+#Creating your own tibble
+# Make 1 tibble
+T1 <- tibble(Site.ID = c("A", "B", "C", "D"), 
+             Temperature = c(14.1, 16.7, 15.3, 12.8))
+T1
+
+# make another tibble
+T2 <-tibble(Site.ID = c("A", "B", "D", "E"), 
+            pH = c(7.3, 7.8, 8.1, 7.9))
+T2
+
+#left_join vs right_join
+#left_join(T1, T2) - joins to T1
+#right_join(T1, T2) - joins to T2
+
+left_join(T1, T2)
+right_join(T1, T2)
+## Joining with by = join_by(Site.ID)
 
 
+#inner_join vs full_join
+# Inner join only keeps the data that is complete in both data sets.
+# Full join keeps everything.
+inner_join(T1, T2)
+full_join(T1, T2)
+## Joining with by = join_by(Site.ID
 
 
+#semi_join vs anti_join
+# How would I find the extra data in y, in this example?
 
 
+#semi_join keeps all rows from the first data set where there are matching values in the second data set, 
+#keeping just columns from the first data set.
+semi_join(T1, T2)
+## Joining with by = join_by(Site.ID
 
-
-
-
+#anti_join Saves all rows in the first data set that do not match anything in the second data set. 
+#This can help you find possible missing data across datasets.
+anti_join(T1, T2)
 
 
 ################################################################################
